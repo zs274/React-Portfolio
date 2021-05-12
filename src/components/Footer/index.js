20fa6afe9ca988e246d4e00d57d4e7ccb84a487f
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-12 01:21:51 +0100

    installed bootstrap, created basic navbar and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto bg-light">
+            Footer
+        </footer>
+    )
+}
+
+export default Footer;
