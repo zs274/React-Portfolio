291d71bab9b6674b5de7f533e69bdc390d5a41e2
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-16 01:41:50 +0100

    created column and container components, edited footer, added links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
+import { IconContext } from "react-icons";
+import { FaGithub, FaLinkedin, FaRegFileAlt } from "react-icons/fa";
+
 
 function Footer() {
     return (
-        <footer className="footer mt-auto bg-light">
-            Footer
-        </footer>
-    )
+        <IconContext.Provider value={{ size: "2em" }}>
+            <footer className="footer">
+                <span className="fa-stack">
+                    <a href="https://github.com/zs274">
+                        <FaGithub /></a> </span>
+                <span className="fa-stack">
+                    <a href="https://www.linkedin.com/in/zarin-salim-0107871bb">
+                        <FaLinkedin /></a></span>
+                <span className="fa-stack">
+                <a href="https://drive.google.com/file/d/13-_HL_4i1glhJXXfCgqHtEcVRuJvAgEe/view?usp=sharing">
+                    <FaRegFileAlt /></a></span>
+            </footer>
+        </IconContext.Provider>
+    );
 }
 
 export default Footer;
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Header from '../components/Header'
 
 function About() {
-    return (
-        <div>
+  return (
+    <div>
+
       <Header backgroundImage="https://source.unsplash.com/95YRwf6CNw8/1920x1280">
         <h1>Hello!</h1>
-        <h2>They're the Good Boys and Girls</h2>
       </Header>
 
-       </div>
-    )
+      <div className="container">
+        <div className="col-12">
+          <h2>About Me</h2>
+          <p id="about"> <br />I am currently studying web development at the University of Birmingham, with the hopes of pursuing a career in this field. After graduating from York St John Univeristy in 2014 with a BA Joint Honours in History and American Studies, I worked in retail for a year before moving to South Korea. I worked as an ESL teacher teaching elementary school children at 3 different schools. This was a very rewarding experience and I gained a lot of skills from it, such as communication and planning.
+            <br /><br />
+            Living in a different country for a year truly enhanced my personality and skillset and it was a very rewarding experience. However, upon my return to the UK I found myself without any real goals for the future. Since graduating I have thought a lot about going back into education, but I never took the leap.
+            <br /><br />
+            That was until I saw an advertisement for the coding bootcamp at the University of Birmingham. I have some experience with HTML and CSS and it has always been a keen interest of mine but I could never find the time or resources to properly learn how to code. I am glad that I decided to take a leap of faith and enrol and I look forward to learning more about coding and web development and starting my career.
+        </p>
+        </div>
+
+      </div>
+    </div>
+
+  )
 }
 
 export default About;
